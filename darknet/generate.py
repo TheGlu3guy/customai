79c89aebfd4dfd86ae6f103635c3e5ffbe07930e
TheGlu3guy <Glueguy@hotmail.fr>
2021-07-03 01:04:03 +0200

    images

--- darknet/generate.py
+++ darknet/generate.py
@@ -1,12 +1,12 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir(os.path.join("data", "taco"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
+        image_files.append("data/taco/" + filename)
 os.chdir("..")
-with open("train.txt", "w") as outfile:
+with open("train_taco.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
